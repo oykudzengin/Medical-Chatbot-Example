951c200075775d8c3b0919dbd4cf83996b8d2954
[oyku zengin] <oykudzengin@gmail.com>
2025-12-08 23:38:24 +0300

    Implement core functionality for medical chatbot: set up Flask app, integrate Pinecone for vector storage, and establish chat interface with Google Generative AI. Added PDF loading, document filtering, and embedding download features.

---
+++ src/helper.py
@@ -0,0 +1,59 @@
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from typing import List
+from langchain.schema import Document
+
+#copy pasting the functions from the jupyter notebook
+
+#Extract data from the pdf file
+def load_pdf_files(data):
+    loader = DirectoryLoader(
+        data,
+        glob="*.pdf",
+        loader_cls=PyPDFLoader
+    )
+
+    documents = loader.load()
+    return documents
+
+def filter_to_minimal_docs(docs: List[Document]) -> List[Document]:
+    """
+    Given a list of Document objects, return a new list of Document objects
+    containing only 'source' in metadata and the original page_content.
+    """
+    minimal_docs: List[Document] = []
+    for doc in docs:
+        src = doc.metadata.get("source")
+        minimal_docs.append(
+            Document(
+                page_content = doc.page_content,
+                metadata={"source": src}
+            )
+        )
+    return minimal_docs
+
+# Split the documents into smaller chunks
+def text_split(minimal_docs):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500, #500 tokens equal to one chunk
+        chunk_overlap=20, #understand the context with this overlap
+    )
+    texts_chunks = text_splitter.split_documents(minimal_docs)
+    return texts_chunks
+
+
+#Download the embeddings from HuggingFace
+def download_embeddings():
+    """
+    Download and return HuggingFace embeddings model.
+    """
+
+    model_name = "sentence-transformers/all-MiniLM-L6-v2"
+    embeddings = HuggingFaceEmbeddings(
+        model_name = model_name,
+    )
+    return embeddings
+
+embedding = download_embeddings()
+
