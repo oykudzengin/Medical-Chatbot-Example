8225d8e4a580bd06840f7f498e0d1af5f69f896e
[oyku zengin] <oykudzengin@gmail.com>
2025-12-11 18:53:57 +0300

    new module added, installed

--- src/helper.py
+++ src/helper.py
@@ -1,5 +1,5 @@
 from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
 from typing import List
 from langchain.schema import Document
