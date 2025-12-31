f33e7d6d20c5606b80812ddfde216066d28849e4
[oyku zengin] <oykudzengin@gmail.com>
2025-12-31 23:43:50 +0300

    new LLM model package dowloaded, app file created

---
+++ app_groq.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, jsonify, request
+from src.helper import download_embeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_groq import ChatGroq
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+from src.prompt import *
+import os
+
+#initialize the flask app
+app = Flask(__name__)
+
+load_dotenv()
+
+PINECONE_API_KEY= os.getenv("PINECONE_API_KEY")
+GROQ_API_KEY= os.getenv("GROQ_API_KEY")
+
+embedding = download_embeddings()
+
+index_name = "medical-chatbot"
+
+docsearch = PineconeVectorStore.from_existing_index(
+    embedding=embedding,
+    index_name=index_name
+)
+
+#creating the chat now
+retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
+#creting the retriever to bring the top 3 most relevant documents
+
+chatModel= ChatGroq(model="llama3-8b-8192",
+                    temperature=0.3,
+                    api_key=GROQ_API_KEY)
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("user", "{input}"),
+    ]
+
+)
+
+question_answer_chain = create_stuff_documents_chain(chatModel, prompt)
+rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+
+
+#creating a basic home route
+@app.route('/')
+def index():
+    return render_template('chat.html')
+
+
+#another route to handle the chat messages
+@app.route('/get', methods=["GET", "POST"])
+def chat():
+    msg= request.form["msg"]
+    input = msg
+    print(input)
+    response = rag_chain.invoke({"input": msg})
+    print("Response: ", response["answer"])
+    return str(response["answer"])
+
+
+
+#execute the app
+if __name__== '__main__':
+    app.run(host="0.0.0.0", port=8081, debug=True)
+
